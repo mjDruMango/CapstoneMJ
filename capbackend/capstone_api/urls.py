aded2df0c4202cf8b8c538ec9b371bccf192aeb8
mjDruMango <mjdrumheller10@gmail.com>
2024-01-16 23:18:23 -0700

    DjangoAPI running

--- capbackend/capstone_api/urls.py
+++ capbackend/capstone_api/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for CapstoneProject project.
+URL configuration for capstone_api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,10 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 
-from . import views
+from capstone_api.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.index, name="index"),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+urlpatterns += router.urls
--- capbackend/capstone_api/wsgi.py
+++ capbackend/capstone_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for CapstoneProject project.
+WSGI config for capstone_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CapstoneProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capstone_api.settings')
 
 application = get_wsgi_application()
